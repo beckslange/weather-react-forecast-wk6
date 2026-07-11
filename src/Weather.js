9f7f7c85b7a63053dbdd7fbf7e39c82666130d59
beckslange <beckslange96@gmail.com>
2026-07-11 14:12:58 -0600

    edited imports in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
-import axios from "axios";
 
 export default function Weather() {
   return (
