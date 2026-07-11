5101b06b11bc113039f822e476c681320da42eef
beckslange <beckslange96@gmail.com>
2026-07-11 14:11:18 -0600

    created structure, styled, added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import "./App.css";
+import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <Weather />
+      <footer>
+        This project was coded by
+        <a
+          href="https://www.linkedin.com/in/rebecca-lange-804b8b111/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rebecca Lange
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/beckslange/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://weather-react-forecast-wk6.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
+import axios from "axios";
+
+export default function Weather() {
+  return (
+    <div className="Weather mb-2">
+      <form className="w-100 justify-content-center">
+        <div className="row mt-1 mb-2">
+          <div className="col-9">
+            <input
+              className="col form-control form-control-lg"
+              type="search"
+              placeholder="Enter a city..."
+              autoFocus
+            />
+          </div>
+          <div className="col-3">
+            <input
+              className="btn btn-primary w-100 btn-lg"
+              type="submit"
+              value="Search"
+            />
+          </div>
+        </div>
+      </form>
+
+      <div className="city-name">
+        <h1>New York</h1>
+      </div>
+      <div className="date">
+        <ul className="list-unstyled">
+          <li>Thursday 11:58</li>
+        </ul>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex align-items-center">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/partly_cloudy_light.svg"
+              alt="Sunny"
+              className="weather-icon me-2"
+            />
+
+            <div className="current-temp">
+              <span className="temperature">11</span>
+              <span className="unit ">°F</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-6">
+          <div className="conditions">
+            <ul className="list-unstyled">
+              <li>Fog</li>
+              <li>Humidity: 93%</li>
+              <li>Wind: 0.87 km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
