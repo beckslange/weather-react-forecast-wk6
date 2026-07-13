3accf33c9732fac88ea7fae1ce38300bbc2c9264
beckslange <beckslange96@gmail.com>
2026-07-13 16:44:37 -0600

    update weatherforecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "bootstrap/dist/css/bootstrap.min.css";
